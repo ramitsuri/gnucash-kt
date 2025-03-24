b95f4a477338ad1404cd1f787013cb4deb9ca1ab
Ramit Suri <ramitsuri007@gmail.com>
2025-03-24 18:36:53 -0400

    Add some gradle setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,17 @@
 plugins {
+    id("application")
     alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.ktlint)
+    alias(libs.plugins.serialization)
+    alias(libs.plugins.shadowJar)
 }
 
 group = "com.ramitsuri"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass= "com.ramitsuri.gnucashreports.MainKt"
+}
 
 repositories {
     mavenCentral()
@@ -14,6 +21,7 @@ dependencies {
     implementation(libs.kotlin.datetime)
     implementation(libs.kotlin.immutable.collections)
     implementation(libs.sqlite.jdbc)
+    implementation(libs.serialization)
 
     testImplementation(kotlin("test"))
 }
@@ -35,4 +43,8 @@ ktlint {
         exclude { element -> element.file.toString().contains("generated/") }
         exclude { element -> element.file.toString().contains("build/") }
     }
+}
+
+tasks.shadowJar {
+    minimize()
 }
