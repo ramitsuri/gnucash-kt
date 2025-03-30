d341a3d60860b2f3bd0d86bb9cdfd3e21cc51c6a
Ramit Suri <ramitsuri007@gmail.com>
2025-03-30 19:29:44 -0400

    Upload app version with the release

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,3 +3,13 @@
 ./gradlew clean
 ./gradlew shadowJar
 mv build/libs/*-all.jar build/libs/app.jar
+
+toml_file="./gradle/libs.versions.toml"
+property="appVersion"
+app_version=$(grep "^$property = " "$toml_file" | sed -E 's/^[^"]*"([^"]*)".*/\1/')
+if [ -z "$app_version" ]; then
+  echo "Error: Property '$property' not found in '$toml_file'"
+  exit 1
+fi
+
+echo "$app_version" > app_version.txt
